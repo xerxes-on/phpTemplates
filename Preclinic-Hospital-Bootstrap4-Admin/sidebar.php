f883011546405e4f3423e6c7f71c829e4ca250a0
Javokhir18 <122219058+Javokhir18@users.noreply.github.com>
2023-10-17 19:27:53 +0500

    added sidebar

---
+++ Preclinic-Hospital-Bootstrap4-Admin/sidebar.php
@@ -0,0 +1,163 @@
+<div class="sidebar" id="sidebar">
+            <div class="sidebar-inner slimscroll">
+                <div id="sidebar-menu" class="sidebar-menu">
+                    <ul>
+                        <li class="menu-title">Main</li>
+                        <li class="active">
+                            <a href="?page=home"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+                        </li>
+						<li>
+                            <a href="?page=doctors"><i class="fa fa-user-md"></i> <span>Doctors</span></a>
+                        </li>
+                        <li>
+                            <a href="?page=patients"><i class="fa fa-wheelchair"></i> <span>Patients</span></a>
+                        </li>
+                        <li>
+                            <a href="?page=appointments"><i class="fa fa-calendar"></i> <span>Appointments</span></a>
+                        </li>
+                        <li>
+                            <a href="?page=schedule"><i class="fa fa-calendar-check-o"></i> <span>Doctor Schedule</span></a>
+                        </li>
+                        <li>
+                            <a href="?page=departments"><i class="fa fa-hospital-o"></i> <span>Departments</span></a>
+                        </li>
+						<li class="submenu">
+							<a href="#"><i class="fa fa-user"></i> <span> Employees </span> <span class="menu-arrow"></span></a>
+							<ul style="display: none;">
+								<li><a href="?page=employees">Employees List</a></li>
+								<li><a href="?page=leaves">Leaves</a></li>
+								<li><a href="?page=holidays">Holidays</a></li>
+								<li><a href="?page=attendance">Attendance</a></li>
+							</ul>
+						</li>
+						<li class="submenu">
+							<a href="#"><i class="fa fa-money"></i> <span> Accounts </span> <span class="menu-arrow"></span></a>
+							<ul style="display: none;">
+								<li><a href="?page=invoices">Invoices</a></li>
+								<li><a href="?page=payments">Payments</a></li>
+								<li><a href="?page=expenses">Expenses</a></li>
+								<li><a href="?page=taxes">Taxes</a></li>
+								<li><a href="?page=provident-fund">Provident Fund</a></li>
+							</ul>
+						</li>
+						<li class="submenu">
+							<a href="#"><i class="fa fa-book"></i> <span> Payroll </span> <span class="menu-arrow"></span></a>
+							<ul style="display: none;">
+								<li><a href="?page=salary"> Employee Salary </a></li>
+								<li><a href="?page=salary-view"> Payslip </a></li>
+							</ul>
+						</li>
+                        <li>
+                            <a href="?page=chat"><i class="fa fa-comments"></i> <span>Chat</span> <span class="badge badge-pill bg-primary float-right">5</span></a>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fa fa-video-camera camera"></i> <span> Calls</span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="?page=voice-call">Voice Call</a></li>
+                                <li><a href="?page=video-call">Video Call</a></li>
+                                <li><a href="?page=incoming-call">Incoming Call</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fa fa-envelope"></i> <span> Email</span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="?page=compose">Compose Mail</a></li>
+                                <li><a href="?page=inbox">Inbox</a></li>
+                                <li><a href="?page=mail-view">Mail View</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fa fa-commenting-o"></i> <span> Blog</span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="?page=blog">Blog</a></li>
+                                <li><a href="?page=blog-details">Blog View</a></li>
+                                <li><a href="?page=add-blog">Add Blog</a></li>
+                                <li><a href="?page=edit-blog">Edit Blog</a></li>
+                            </ul>
+                        </li>
+						<li>
+							<a href="?page=assets"><i class="fa fa-cube"></i> <span>Assets</span></a>
+						</li>
+						<li>
+							<a href="?page=activities"><i class="fa fa-bell-o"></i> <span>Activities</span></a>
+						</li>
+						<li class="submenu">
+							<a href="#"><i class="fa fa-flag-o"></i> <span> Reports </span> <span class="menu-arrow"></span></a>
+							<ul style="display: none;">
+								<li><a href="?page=expense-reports"> Expense Report </a></li>
+								<li><a href="?page=invoice-reports"> Invoice Report </a></li>
+							</ul>
+						</li>
+                        <li>
+                            <a href="?page=settings"><i class="fa fa-cog"></i> <span>Settings</span></a>
+                        </li>
+                        <li class="menu-title">UI Elements</li>
+                        <li class="submenu">
+                            <a href="#"><i class="fa fa-laptop"></i> <span> Components</span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="?page=uikit">UI Kit</a></li>
+                                <li><a href="?page=typography">Typography</a></li>
+                                <li><a href="?page=tabs">Tabs</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fa fa-edit"></i> <span> Forms</span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="?page=form-basic-inputs">Basic Inputs</a></li>
+                                <li><a href="?page=form-input-groups">Input Groups</a></li>
+                                <li><a href="?page=form-horizontal">Horizontal Form</a></li>
+                                <li><a href="?page=form-vertical">Vertical Form</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fa fa-table"></i> <span> Tables</span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="?page=tables-basic">Basic Tables</a></li>
+                                <li><a href="?page=tables-datatables">Data Table</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="?page=calendar"><i class="fa fa-calendar"></i> <span>Calendar</span></a>
+                        </li>
+                        <li class="menu-title">Extras</li>
+                        <li class="submenu">
+                            <a href="#"><i class="fa fa-columns"></i> <span>Pages</span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li><a href="?page=login"> Login </a></li>
+                                <li><a href="?page=register"> Register </a></li>
+                                <li><a href="?page=forgot-password"> Forgot Password </a></li>
+                                <li><a href="?page=change-password2"> Change Password </a></li>
+                                <li><a href="?page=lock-screen"> Lock Screen </a></li>
+                                <li><a href="?page=profile"> Profile </a></li>
+                                <li><a href="?page=gallery"> Gallery </a></li>
+                                <li><a href="?page=error-404">404 Error </a></li>
+                                <li><a href="?page=error-500">500 Error </a></li>
+                                <li><a href="?page=blank-page"> Blank Page </a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="javascript:void(0);"><i class="fa fa-share-alt"></i> <span>Multi Level</span> <span class="menu-arrow"></span></a>
+                            <ul style="display: none;">
+                                <li class="submenu">
+                                    <a href="javascript:void(0);"><span>Level 1</span> <span class="menu-arrow"></span></a>
+                                    <ul style="display: none;">
+                                        <li><a href="javascript:void(0);"><span>Level 2</span></a></li>
+                                        <li class="submenu">
+                                            <a href="javascript:void(0);"> <span> Level 2</span> <span class="menu-arrow"></span></a>
+                                            <ul style="display: none;">
+                                                <li><a href="javascript:void(0);">Level 3</a></li>
+                                                <li><a href="javascript:void(0);">Level 3</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="javascript:void(0);"><span>Level 2</span></a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);"><span>Level 1</span></a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
